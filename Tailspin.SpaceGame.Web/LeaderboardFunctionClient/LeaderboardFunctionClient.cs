5bb1a8037dcf530c9898c4743ad05827731f6125
John <333jzs@gmail.com>
2022-04-30 15:27:57 +1000

    Update LeaderboardFunctionClient.cs

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -20,8 +20,8 @@ namespace TailSpin.SpaceGame.Web
         {
             using (WebClient webClient = new WebClient())
             {
-                string json0 = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
-                string json = await webClient.,DownloadStringTaskAsync("http://tailspin-space-game-leaderboard-20361.azurewebsites.net/api/LeaderboardFunction?page=&pageSize=10&mode=");
+                //string json0 = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
+                string json = await webClient.DownloadStringTaskAsync("http://tailspin-space-game-leaderboard-20361.azurewebsites.net/api/LeaderboardFunction?page=&pageSize=10&mode=");
                 return JsonSerializer.Deserialize<LeaderboardResponse>(json);
             }
         }
