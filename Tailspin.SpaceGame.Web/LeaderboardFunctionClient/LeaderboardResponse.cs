264752a8b4f1ec7133807513aa8adf34deab6afa
John <333jzs@gmail.com>
2022-05-01 11:25:27 +1000

    Change capitalization in Leaderboardresponse class to match the json that we are deserializing

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardResponse.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardResponse.cs
@@ -6,17 +6,17 @@ namespace TailSpin.SpaceGame.Web
     public class LeaderboardResponse
     {
         // The game mode selected in the view.
-        public string SelectedMode { get; set; }
+        public string selectedMode { get; set; }
         // The game region (map) selected in the view.
-        public string SelectedRegion { get; set; }
+        public string selectedRegion { get; set; }
         // The current page to be shown in the view.
-        public int Page { get; set; }
+        public int page { get; set; }
         // The number of items to show per page in the view.
-        public int PageSize { get; set; }
+        public int pageSize { get; set; }
 
         // The scores to display in the view.
         public IEnumerable<ScoreProfile> Scores { get; set; }
         // The total number of results for the selected game mode and region in the view.
-        public int TotalResults { get; set; }
+        public int totalResults { get; set; }
     }
 }
